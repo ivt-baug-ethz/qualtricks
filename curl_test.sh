17c80823c01cb3f495c924e282cabda513937f71
Daniel Heimgartner <d.heimgartners@gmail.com>
2022-03-13 21:43:42 +0100

    removed token

--- curl_test.sh
+++ curl_test.sh
@@ -1,7 +1,7 @@
 curl --request POST \
-  --url https://fra1.qualtrics.com/API/v3/mailinglists/ML_cROpOLdqoh22Tyu/contacts \
+  --url https://<base_url>/API/v3/mailinglists/ML_cROpOLdqoh22Tyu/contacts \
   --header 'Content-Type: application/json' \
-  --header 'X-API-TOKEN: WmUIBKhzXPftK2g1MNEDcMtqqWyUKN3V4bXkIXgi' \
+  --header 'X-API-TOKEN: <token>' \
   --data '{
   "firstName": "hello",
   "lastName": "world",
